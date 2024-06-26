865cc588a8e475841c575feecbad6dcea67ec125
NeuralFlux <40491005+NeuralFlux@users.noreply.github.com>
2024-06-26 14:25:20 -0400

    parsing whole file

--- parser.py
+++ parser.py
@@ -15,7 +15,7 @@ def load_substances(data_folder: str):
     file_name = os.path.join(data_folder, "dump-public-2023-12-14.gsrs")
 
     with gzip.GzipFile(file_name) as fd:
-        for raw_line in itertools.islice(fd, 1):
+        for raw_line in fd:
             record = json.loads(raw_line.decode("utf-8").strip())
             record = dict_convert(record, keyfn=process_key)
             record = dict_sweep(record, vals=["", None], remove_invalid_list=True)
