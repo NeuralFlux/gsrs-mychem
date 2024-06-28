e49989f5c607f3b383d9fa8e7e0e21429dedb7ec
NeuralFlux <40491005+NeuralFlux@users.noreply.github.com>
2024-06-28 11:58:04 -0400

    format dates using dict_traverse

--- parser.py
+++ parser.py
@@ -1,26 +1,25 @@
 import gzip
-import itertools
 import json
 import os
 from datetime import datetime, timezone
-from typing import List, Tuple
+from typing import Any, List
 
 from biothings import config
-from biothings.utils.dataload import dict_convert, dict_sweep
+from biothings.utils.dataload import dict_convert, dict_sweep, dict_traverse
 
 logging = config.logger
 
 process_key = lambda key: key.replace(" ", "_").lower()
 recognized_code_systems = ["cas", "chebi", "chembl", "drugbank", "fdaunii", "mesh", "pubchem"]
+date_cols = ("documentDate", "deprecatedDate")
 
 
-def timestamp_to_date(d: dict, keys: Tuple[str]):
-    """"""
-    for key in keys:
-        if key in d.keys():
-            date_obj = datetime.fromtimestamp(int(d[key]), tz=timezone.utc)
-            d.update({key: date_obj.strftime("%Y-%m-%d")})
-    return d
+def timestamp_to_date(k: str, v: Any):
+    """check if a key-value pair needs date formatting and do so"""
+    if k in date_cols:
+        date_obj = datetime.fromtimestamp(int(v), tz=timezone.utc)
+        v = date_obj.strftime("%Y-%m-%d")
+    return k, v
 
 
 def parse_xrefs(codes: List[str]):
@@ -52,7 +51,7 @@ def load_substances(data_folder: str):
             record = dict_convert(record, keyfn=process_key)
             if "codes" in record.keys():
                 record["xrefs"] = parse_xrefs(record["codes"])
-            # record = timestamp_to_date(record, ("documentDate", "deprecatedDate"))
+            dict_traverse(record, timestamp_to_date)  # parse dates in `date_cols` only
             record = dict_sweep(record, vals=["", None], remove_invalid_list=True)
 
             _id = f"gsrs.uuid:{record['uuid']}"
